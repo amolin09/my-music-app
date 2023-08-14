3d5253526826fe7235cd21f797aa04b49bcc1117
amolin09 <alex.molina5829@gmail.com>
2023-08-13 23:49:12 -0500

    added dashboard components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import Dashboard from './Components/Dashboard'
+
+import {useState, useEffect} from 'react'
+
+import {Button, Toolbar} from '@mui/material'
+
+import {TextField} from '@mui/material'
+
+import {Typography} from '@mui/material';
+
+import {AppBar} from '@mui/material';
+
+import {Box} from '@mui/material';
+
+function App(){
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const isLoggedInHandler = () =>{
+    let status = isLoggedIn === true ? false : true;
+    setIsLoggedIn(status);
+  }
+
+
+  if(isLoggedIn){
+   return(
     <div className="App">
+      <Box sx={{flexGrow : 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            Music App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      </Box>
+    
+    <Dashboard/>
+    
+  </div>
+ 
+   )
+    
+}
+
+else{
+
+  return (
+   <div className="App">
+     <Box sx={{flexGrow : 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            Music App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      </Box>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Login</h2>
+        <TextField autoComplete="off" id="username" label="Username" variant="filled" />
+        <br></br>
+        <TextField  autoComplete="off" id="pass" label ="Password" variant='filled'/>
+        <br></br>
+        <Button variant="contained" onClick={isLoggedInHandler}>Submit</Button>
       </header>
     </div>
   );
+
+}
+  
 }
 
 export default App;
